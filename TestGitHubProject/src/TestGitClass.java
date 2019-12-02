a7f7d13512d1e2481bb1ab0d3b08edd6f7bb01e1
nawshad <mohammad.bhuiyan77@gmail.com>
2019-12-01 23:11:04 -0500

    commiting first time

---
+++ TestGitHubProject/src/TestGitClass.java
@@ -0,0 +1,17 @@
+
+public class TestGitClass {
+
+	public static void sum() {
+		int a = 10;
+		int b = 20;
+		int sum = a + b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello nawshad");
+		TestGitClass.sum();
+
+	}
+
+}
