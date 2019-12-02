da4d144a20e2e241ac294ad3f0bd45b8c520a467
User <User@DESKTOP-S97MK9B>
2019-12-01 23:32:46 -0500

    added b comment

--- TestGitHubProject/src/TestGitClass.java
+++ TestGitHubProject/src/TestGitClass.java
@@ -3,7 +3,7 @@ public class TestGitClass {
 
 	public static void sum() {
 		int a = 10; // a holding 10
-		int b = 20;
+		int b = 20; // b holding 20
 		int sum = a + b;
 	}
 	public static void main(String[] args) {
